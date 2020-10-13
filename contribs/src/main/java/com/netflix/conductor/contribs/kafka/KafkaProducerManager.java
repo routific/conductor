8abe8389ecaebd84f5c6a7f754651ad0f2150ee3
pmchung <philip@pmhc.co>
2020-10-12 22:48:26 -0700

    Authenticate kafka publish task

--- contribs/src/main/java/com/netflix/conductor/contribs/kafka/KafkaProducerManager.java
+++ contribs/src/main/java/com/netflix/conductor/contribs/kafka/KafkaProducerManager.java
@@ -9,6 +9,9 @@ import com.netflix.conductor.core.config.Configuration;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.common.config.SaslConfigs;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -40,14 +43,32 @@ public class KafkaProducerManager {
 	private static final String KAFKA_PUBLISH_MAX_BLOCK_MS = "kafka.publish.max.block.ms";
 	private static final String DEFAULT_MAX_BLOCK_MS = "500";
 
+	private static final String KAFKA_PUBLISH_SECURITY_PROTOCOL = "kafka.publish.security.protocol";
+	private static final String DEFAULT_SECURITY_PROTOCOL = null;
+
+	private static final String KAFKA_PUBLISH_SASL_MECHANISM = "kafka.publish.sasl.mechanism";
+	private static final String DEFAULT_SASL_MECHANISM = "PLAIN";
+
+	private static final String KAFKA_PUBLISH_SASL_USERNAME = "kafka.publish.sasl.username";
+	private static final String KAFKA_PUBLISH_SASL_PASSWORD = "kafka.publish.sasl.password";
 
 	public final String requestTimeoutConfig;
 	private Cache<Properties, Producer> kafkaProducerCache;
 	private final String maxBlockMsConfig;
 
+	private final String securityProtocolConfig;
+	private final String saslMechanismConfig;
+	private final String saslUsernameConfig;
+	private final String saslPasswordConfig;
+
 	public KafkaProducerManager(Configuration configuration) {
 		this.requestTimeoutConfig = configuration.getProperty(KAFKA_PUBLISH_REQUEST_TIMEOUT_MS, DEFAULT_REQUEST_TIMEOUT);
 		this.maxBlockMsConfig = configuration.getProperty(KAFKA_PUBLISH_MAX_BLOCK_MS, DEFAULT_MAX_BLOCK_MS);
+		this.securityProtocolConfig = configuration.getProperty(KAFKA_PUBLISH_SECURITY_PROTOCOL, DEFAULT_SECURITY_PROTOCOL);
+		this.saslMechanismConfig = configuration.getProperty(KAFKA_PUBLISH_SASL_MECHANISM, DEFAULT_SASL_MECHANISM);
+		this.saslUsernameConfig = configuration.getProperty(KAFKA_PUBLISH_SASL_USERNAME, "");
+		this.saslPasswordConfig = configuration.getProperty(KAFKA_PUBLISH_SASL_PASSWORD, "");
+
 		int cacheSize = configuration.getIntProperty(KAFKA_PRODUCER_CACHE_SIZE, DEFAULT_CACHE_SIZE);
 		int cacheTimeInMs = configuration.getIntProperty(KAFKA_PRODUCER_CACHE_TIME_IN_MILLIS, DEFAULT_CACHE_TIME_IN_MILLIS);
 		this.kafkaProducerCache = CacheBuilder.newBuilder().removalListener(LISTENER)
@@ -86,16 +107,23 @@ public class KafkaProducerManager {
 		if (Objects.nonNull(input.getRequestTimeoutMs())) {
 			requestTimeoutMs = String.valueOf(input.getRequestTimeoutMs());
 		}
-		
+
 		String maxBlockMs = maxBlockMsConfig;
 
 		if (Objects.nonNull(input.getMaxBlockMs())) {
 			maxBlockMs = String.valueOf(input.getMaxBlockMs());
 		}
-		
+
 		configProperties.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeoutMs);
 		configProperties.put(ProducerConfig.MAX_BLOCK_MS_CONFIG, maxBlockMs);
 		configProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, STRING_SERIALIZER);
+
+		if (Objects.nonNull(securityProtocolConfig)) {
+			configProperties.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, securityProtocolConfig);
+			configProperties.put(SaslConfigs.SASL_MECHANISM, saslMechanismConfig);
+			configProperties.put(SaslConfigs.SASL_JAAS_CONFIG, "org.apache.kafka.common.security.plain.PlainLoginModule required username=\""+saslUsernameConfig+"\" password=\""+saslPasswordConfig+"\";");
+		}
+
 		return configProperties;
 	}
 }
