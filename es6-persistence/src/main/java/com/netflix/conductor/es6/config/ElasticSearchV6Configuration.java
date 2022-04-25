e1f3f409385f51bc60a98c9f4b5411e1acc1f692
pmchung <philip@pmhc.co>
2022-04-24 19:16:53 -0700

    ES6/7 ssl cert noop

--- es6-persistence/src/main/java/com/netflix/conductor/es6/config/ElasticSearchV6Configuration.java
+++ es6-persistence/src/main/java/com/netflix/conductor/es6/config/ElasticSearchV6Configuration.java
@@ -20,11 +20,17 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.net.ssl.SSLContext;
+
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.SSLContexts;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
@@ -108,6 +114,27 @@ public class ElasticSearchV6Configuration {
                     AuthScope.ANY,
                     new UsernamePasswordCredentials(
                             properties.getUsername(), properties.getPassword()));
+
+            try {
+                SSLContextBuilder sslBuilder =
+                        SSLContexts.custom().loadTrustMaterial(null, (x509Certificates, s) -> true);
+                final SSLContext sslContext = sslBuilder.build();
+
+                return builder.setHttpClientConfigCallback(
+                        new RestClientBuilder.HttpClientConfigCallback() {
+                            @Override
+                            public HttpAsyncClientBuilder customizeHttpClient(
+                                    HttpAsyncClientBuilder httpClientBuilder) {
+                                return httpClientBuilder
+                                        .setDefaultCredentialsProvider(credentialsProvider)
+                                        .setSSLContext(sslContext)
+                                        .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE);
+                            }
+                        });
+            } catch (Exception e) {
+                log.error("Error authenticating to ElasticSearch", e.getMessage());
+            }
+
             builder.setHttpClientConfigCallback(
                     httpClientBuilder ->
                             httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
