d1b7f7417589975e28f7f5c3c531e0b29313e5a6
pmchung <philip@pmhc.co>
2023-02-17 02:07:05 -0800

    Tracing setup

---
+++ core/src/main/java/com/netflix/conductor/core/tracing/Tracing.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2023 Netflix, Inc.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package com.netflix.conductor.core.tracing;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.StatusCode;
+
+public class Tracing {
+    private static final Logger log = LoggerFactory.getLogger(Tracing.class);
+
+    private Optional<Span> span = Optional.empty();
+
+    public Tracing(Optional<Span> span) {
+        this.span = span;
+
+        if (span.isPresent()) {
+            log.info("Span sampled: {}", span.get().getSpanContext().getTraceFlags().isSampled());
+        }
+    }
+
+    public void finish() {
+        if (this.span.isPresent()) {
+            Span span = this.span.get();
+
+            span.setStatus(StatusCode.OK);
+            span.end();
+        }
+    }
+}
